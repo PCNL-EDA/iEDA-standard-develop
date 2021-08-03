4b93f5647dc5f1d4c71ca5190c8a27bf4b5a1221
shengkunwu <shengkunwu@foxmail.com>
2021-08-03 10:24:28 +0800

    shengkunwu

---
+++ CPP-Program-Assignment/Assignment2/code-submit/shengkunwu/gtest/gbuild/CMakeFiles/gexe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gexe.dir/home/wsk/myfile/work/2021_9/iEDA-standard-develop/CPP-Program-Assignment/Assignment2/code-submit/shengkunwu/src/graph.cpp.o"
+  "CMakeFiles/gexe.dir/home/wsk/myfile/work/2021_9/iEDA-standard-develop/CPP-Program-Assignment/Assignment2/code-submit/shengkunwu/src/node.cpp.o"
+  "CMakeFiles/gexe.dir/testgraph.cpp.o"
+  "CMakeFiles/gexe.dir/testnode.cpp.o"
+  "gexe"
+  "gexe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gexe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
