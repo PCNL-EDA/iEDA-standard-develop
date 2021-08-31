c108879cc0dac6e58aadae1ad367c1513f2b350b
harrywh <harry0789@qq.com>
2021-08-31 14:51:07 +0800

    refine the program skeleon

--- CPP-Program-Assignment/Assignment1/code-submit/3th/wanghao/README.md
+++ CPP-Program-Assignment/Assignment1/code-submit/3th/wanghao/README.md
@@ -1,7 +1,7 @@
 ## Build
 To build this project, please run the following command in shell:
 ```shell
-bash build.sh
+bash build.sh ["binary_install_path"]  # set install path if you need, not necessarily
 ```
 ## Run
 To run this program, please run the following command in shell:
--- CPP-Program-Assignment/Assignment1/code-submit/3th/wanghao/build.sh
+++ CPP-Program-Assignment/Assignment1/code-submit/3th/wanghao/build.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-echo -e "\033[47;30m               program build start               \033[0m"
 set -e
-rm -rf ./build
-mkdir -p ./build
-cd ./build
-cmake ../src
+echo -e "\033[47;30m               program build start               \033[0m"
+BASEPATH=$(pwd)
+
+if [ "$#" == 0 ]
+then
+  echo -e "\033[47;30m The executable will be installed in directory:  \033[0m"
+  echo -e "\033[46;30m$BASEPATH/build/bin\033[0m"
+  INSTALL_DIR="$BASEPATH/build"
+elif [ "$#" == 1 ]
+then
+  echo -e "\033[47;30m The executable will be installed in directory:  \033[0m"
+  echo -e "\033[46;30m$1\033[0m"
+  INSTALL_DIR="$BASEPATH/$1"
+else
+  echo -e "Please either provide an install directory path"
+  echo -e "or this program will be installed in default directory:"
+  echo -e "\033[47;30m./build/bin\033[0m"
+  echo -e "\033[47;30m             program build failed              \033[0m"
+  exit 1
+fi
+
+rm -rf $BASEPATH/build
+mkdir -p $BASEPATH/build/cmake_cache
+cd $BASEPATH/build/cmake_cache
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR $BASEPATH
 make
-mv hello ../bin
-echo -e "\033[47;30m    run ./bin/hello' to execute the program!    \033[0m"
+make install
+echo -e "\033[47;30m run $INSTALL_DIR/bin/hello' to execute the program! \033[0m"
