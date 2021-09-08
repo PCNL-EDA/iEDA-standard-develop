c237b00befc6104e817dc882f5816c339e48ef16
harrywh <harry0789@qq.com>
2021-09-08 20:51:04 +0800

    skeleton copy from assignment2

---
+++ CPP-Program-Assignment/Assignment2/code-submit/3th/wanghao/include/graph_matrix.h
@@ -0,0 +1,61 @@
+/**
+ * @file          graph_matrix.h
+ * @brief         Class GraphMatrix provides Eulerian Path algorithm
+ * @author        wanghao (harry0789@qq.com)
+ * @date          2021-09-05
+ * @copyright     Copyright (c) 2021 PCL IEDA
+ */
+#ifndef EULER_H_
+#define EULER_H_
+#include <cstdio>
+#include <cstring>
+
+// dimension manipulation, eg: unsigned* -> unsigned**
+#define visit(i, j) visited_edge[i * _size + j]
+
+namespace Assignment2 {
+/**
+ * @brief
+ *
+ * Class GraphMatrix stores graph matrix
+ * and provides Eulerian Path algorithm
+ *
+ */
+class GraphMatrix {
+ public:
+  GraphMatrix()               = default;
+  GraphMatrix(GraphMatrix &)  = delete;
+  GraphMatrix(GraphMatrix &&) = delete;
+  ~GraphMatrix() {
+    if (_graph_matrix != nullptr) {
+      delete[] _graph_matrix;
+    }
+  }
+  void operator=(const GraphMatrix &) = delete;
+
+  // get
+  inline unsigned get_graph_size() const { return _size; }
+
+  inline unsigned &graph_edge(unsigned i, unsigned j) const { return _graph_matrix[i * _size + j]; }
+
+  // set
+  void read_file(FILE *);
+
+  void printAll() const;
+  bool isConnectedGraph() const;
+  bool isDegreeInEqualsOut() const;
+  bool eulerianPath(unsigned *, unsigned *) const;
+  void resetGraphSpace(unsigned);
+
+  inline bool isEulerianPathExist() const { return (isConnectedGraph() && isDegreeInEqualsOut()) ? true : false; }
+
+ private:
+  void dfs_visit(unsigned, unsigned *) const;
+  void dfs_order(unsigned, unsigned *, unsigned *, unsigned *) const;
+
+  unsigned *_graph_matrix;
+  unsigned  _size;
+};
+
+}  // namespace Assignment2
+#endif
